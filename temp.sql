e119870b74f96eac3a6dd590295bcda8c4aa77fd
Riaz Syed <87838857+riaz7se@users.noreply.github.com>
2024-11-29 22:30:11 +0530

    Create temp.sql

---
+++ temp.sql
@@ -0,0 +1,12 @@
+
+-- Step 1: Identify all constraints and indexes
+SELECT constraint_name, constraint_type 
+FROM user_constraints 
+WHERE table_name = 'YOUR_TABLE_NAME' AND 
+      (column_name = 'YOUR_COLUMN_NAME' OR 
+       constraint_type IN ('P', 'R', 'U'));
+
+-- Step 2: Identify all indexes on the column
+SELECT index_name 
+FROM user_indexes 
+WHERE table_name = 'YOUR_TABLE_NAME';
